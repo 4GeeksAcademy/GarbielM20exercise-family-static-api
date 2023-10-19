0379dae1379dac57d27d84d04bd4680ffc5942ab
Gabriel Montaño <134933872+GabrielM20-1@users.noreply.github.com>
2023-10-19 19:15:49 +0000

    feed:1version

--- src/datastructures.py
+++ src/datastructures.py
@@ -13,7 +13,24 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [{"id": 1,
+                        "first_name0":'Jhon',
+                        "last_nsmr":self.last_name,
+                        "age":33,
+                         "lucky_numbers":[7,13,22]},
+                        {"id": self._generateId,
+                        "first_name0":'Jane',
+                        "last_nsmr":self.last_name,
+                        "age":35,
+                         "lucky_numbers":[10,14,3]},
+                        {"id": self._generateId,
+                           "first_name0":'Jimmy',
+                          "last_nsmr":self.last_name,
+                         "age":5,
+                          "lucky_numbers":[1]},
+        ]
+
+
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -21,16 +38,35 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        member['last_name'] = self.last_name
+        return self._members
+        
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        result = []
+        for member in self._members:
+            if member["id"] != id:
+                result.append(member)
+                self._members = result
+        return result
+        
 
     def get_member(self, id):
-        # fill this method and update the return
+        # Fill this method and update the return
+        #opcion list compr
         pass
+        # Opcion2- for standard
+        result = []
+        for memeber in self._members:
+            if member["id"] == id:
+                result.append(memeber)
+        return result
 
-    # this method is done, it returns a list with all the family members
+
+    # This method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+
+
+
+
